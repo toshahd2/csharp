464ffc517e692d62b259c9537ce28eba5c1f6aa1
Anton <a@aevip.ru>
2022-07-18 22:05:01 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите значение для проверки: ");
+string num = Console.ReadLine();
+int len = num.Length;
+for (int i = 0; i < (num.Length/2); i++)
+{
+    if (num[i] != num[num.Length - 1 - i])
+    {
+        Console.WriteLine($"Число {num} - не палиндром");
+        break;
+    }
+Console.Clear();
+Console.WriteLine($"{num} - палиндром");
+}
